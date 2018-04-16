44b68ca32c8cd1135cb207f5b5b4e693f279a1f5
Alex Kim <askim1019@gmail.com>
2018-04-16 01:31:13 -0500

    Add logic and loop through answer submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,57 @@
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
+var choice;
+var choiceArray = ['choices1', 'choices2', 'choice3'] 
 
-var questions = [
-    {}
-]
+function startGame() {
+    $("#startBtn").click(function(){
+        $("#startBtn").hide();
+        $("#questionsDiv").show();
+    });
+}
+
+
+function submitAnswers() {
+    $("#submitBtn").click(function(){
+        for (var i = 0; i < choiceArray.length; i++) {
+            choice = $('input[name=' + choiceArray[i] + ']:checked').val();
+            
+            if (choice === 'correct') {
+                correctAnswers++;
+                console.log(correctAnswers);
+            } else if (choice === 'incorrect') {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            } else {
+                unanswered++;
+                console.log(unanswered);
+            }
+        }
+    });
+}
+/* 
+function checkCorrect(){
+    $("#submitBtn").click(function(){
+        firstChoice = $('input[name=choices1]:checked').val();
+        console.log(firstChoice);
+        
+        if (firstChoice === 'correct') {
+            correctAnswers++;
+            console.log(correctAnswers);
+        } else if (firstChoice === 'incorrect') {
+            incorrectAnswers++;
+            console.log(incorrectAnswers);
+        } else {
+            unanswered++;
+            console.log(unanswered);
+        }
+    });
+} */
 
 $(document).ready(function(){
+    $("#questionsDiv").hide();
 
+    startGame();
+    submitAnswers();
 });
