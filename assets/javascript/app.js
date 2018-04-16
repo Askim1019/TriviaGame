f03706a7df49aaa6ab6fe0f689f6d0fdb39b319d
Alex Kim <askim1019@gmail.com>
2018-04-15 22:09:26 -0500

    initialize repo and layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+// Make a start button event to start the game and the timer
+
+// Make about 10 questions
+
+// Correct Answers
+
+// Incorrect Answers
+
+// Unanswered Answers
+
+// Add a done button at bottom when you finish early
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+var questions = [
+    {}
+]
+
+$(document).ready(function(){
+
+});
